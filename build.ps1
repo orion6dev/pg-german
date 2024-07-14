ce312e95634a37bf82fdcd22e4f84c4ccd410af2
André Jager <andre.jager@orion6.nl>
2024-07-14 17:32:31 +0200

    Add docker build configuration to build.ps1
    The build.ps1 file has been created and populated with Docker build configuration code. It enables Docker to build using the specified build arguments, leveraging environment variables and pre-set argument values.

---
+++ build.ps1
@@ -0,0 +1,8 @@
+﻿docker build `
+  --build-arg GITHUB_USER=$env:GH_USER `
+  --build-arg GITHUB_TOKEN=$env:GH_TOKEN `
+  --build-arg BUILD_CONFIGURATION=Release `
+  --build-arg INF_VER='local.build' `
+  --build-arg SEMVER='1' `
+  --build-arg RELEASE_TAG='T' `
+  -t ghcr.io/orion6dev/pg-german:local .
